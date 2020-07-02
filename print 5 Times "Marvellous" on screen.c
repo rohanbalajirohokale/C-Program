b7d9f750b80865c05125cb8c9ba1ed3cec38ca40
rohanbalajirohokale <67697472+rohanbalajirohokale@users.noreply.github.com>
2020-07-02 10:53:53 +0530

    Create print 5 Times "Marvellous" on screen.c

---
+++ print 5 Times "Marvellous" on screen.c
@@ -0,0 +1,15 @@
+//2.Program to print 5 Times "Marvellous" on screen.
+#include<stdio.h>
+void display()
+{
+	int i=0;
+	for(i=1;i<=5;i++)
+	{
+		printf("Marvellous\n");
+	}
+}
+int main()
+{
+	display();
+		return 0;
+}
