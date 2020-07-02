b8db97750d2f48c400e4b9cea81da3b136b6c725
rohanbalajirohokale <67697472+rohanbalajirohokale@users.noreply.github.com>
2020-07-02 11:33:01 +0530

    Create Program to print 5 to 1 numbers on screen.c

---
+++ Program to print 5 to 1 numbers on screen.c
@@ -0,0 +1,16 @@
+//3. Program to print 5 to 1 numbers on screen.
+#include<stdio.h>
+void display()
+{
+	int i=5;
+	while(i>0)
+	{
+		printf("%d\n",i);
+		i--;
+	}
+}
+int main()
+{
+	display();
+	return 0;
+}
